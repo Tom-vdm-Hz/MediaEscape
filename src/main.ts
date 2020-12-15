565d096447635c5e85073a1d88ce72131253d535
Lisa Mondriaan <lmondriaan@hotmail.com>
2020-12-15 14:45:06 +0100

    changes startscreen dinges

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
 console.log("Javascript is working!");
+document.getElementById("canvas").classList.add("hidden");
+let myStartButton = document.getElementById("startButton");
 
 // Add EventListener to load the game whenever the browser is ready
-window.addEventListener('load', () => {
-    console.log("Handling the Load event");
-
+myStartButton.addEventListener('click', () => {
+    console.log("starting game!");
+    document.getElementById("canvas").classList.remove("hidden");
+    document.getElementById("canvas").classList.add("visible");
     const game = new Game(document.getElementById('canvas'));
 });
-
-
-
-
-
