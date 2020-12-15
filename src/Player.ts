2daad4539aa1c707d9b5a66d9f8331458302fba7
Roemer <bare0025@hz.nl>
2020-12-15 12:27:24 +0100

    created move method

--- src/Player.ts
+++ src/Player.ts
@@ -1,4 +1,30 @@
 class Player {
     private name: string
     private collectedCodes: number[]
+    private keyListener: KeyListener
+
+    public constructor() {
+        this.keyListener = new KeyListener
+
+        
+    }
+
+    public move() {
+        //left key is pressed
+        if (this.keyListener.isKeyDown(37) == true) {
+            console.log('left')
+        }
+        //up key is pressed
+        if (this.keyListener.isKeyDown(38) == true) {
+            console.log('up')
+        }
+        //right key is pressed
+        if (this.keyListener.isKeyDown(39) == true) {
+            console.log('right')
+        }
+        //down key is pressed
+        if (this.keyListener.isKeyDown(40) == true) {
+            console.log('down')
+        }
+    }
 }
