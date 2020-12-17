ebd7ad399727499cb87840ffb7dacf942ba3658a
Tom van den Muijsenberg <tom.v.d.muijsenberg@gmail.com>
2020-12-17 11:14:27 +0100

    Auto stash before merge of "develop" and "origin/develop"

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,16 @@ document.getElementById("canvas").classList.add("hidden");
 let myStartButton = document.getElementById("startButton");
 
 // Add EventListener to load the game whenever the browser is ready
+
 myStartButton.addEventListener('click', () => {
     console.log("starting game!");
     document.getElementById("canvas").classList.remove("hidden");
     document.getElementById("canvas").classList.add("visible");
-    const game = new Game(document.getElementById('canvas'), 'a', 'A');
+    const game = new Game(document.getElementById('canvas'), 'a', 'A', window.innerHeight, window.innerWidth);
+
 });
 
-// @ts-ignore
-function getMousePosition(canvas, event) {
+function getMousePosition(canvas: HTMLCanvasElement, event: MouseEvent) {
     let rect = canvas.getBoundingClientRect();
     let x = event.clientX - rect.left;
     let y = event.clientY - rect.top;
