2840141dff0e219b682c970189d0be3d95880b19
Tom van den Muijsenberg <tom.v.d.muijsenberg@gmail.com>
2021-01-18 10:13:51 +0100

    removed another unused class

--- build/app.js
+++ build/app.js
@@ -974,24 +974,6 @@ class RoomSky403 extends Room {
         super(room, questions, clickableItems, roomNumber);
     }
 }
-class Size {
-    constructor(width, height) {
-        this._width = width;
-        this._height = height;
-    }
-    get width() {
-        return this._width;
-    }
-    set width(value) {
-        this._width = value;
-    }
-    get height() {
-        return this._height;
-    }
-    set height(value) {
-        this._height = value;
-    }
-}
 class Vault extends View {
     constructor(img) {
         super(img);
